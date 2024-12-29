b419c909960f943abf3a64329afec450c0637ccf
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-29 10:24:55 -0600

    Update App.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
+
+import React from 'react';
 import Onboarding from './Onboarding';
 
 function App() {
