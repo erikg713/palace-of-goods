2615774ee69a0c54f153e50e803fbb2502f740de
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-29 10:38:12 -0600

    Update Onboarding.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Onboarding.js
+++ frontend/src/components/Onboarding.js
@@ -1,5 +1,32 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { completeOnboarding } from './redux/onboardingSlice';
+import { Link } from 'react-router-dom';
+
+const Onboarding = () => {
+  const onboardingCompleted = useSelector((state) => state.onboarding.completed);
+  const dispatch = useDispatch();
+
+  const handleComplete = () => {
+    dispatch(completeOnboarding());
+  };
+
+  return (
+    <div>
+      <h1>Onboarding Page</h1>
+      {onboardingCompleted ? (
+        <p>Onboarding is already completed!</p>
+      ) : (
+        <button onClick={handleComplete}>Complete Onboarding</button>
+      )}
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default Onboarding;
 
 function Onboarding() {
     const [walletAddress, setWalletAddress] = useState("");
