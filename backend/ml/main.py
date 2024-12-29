3149412f623bf1854b9e6d0a3d2d7a4cef3c57dd
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-29 09:38:10 -0600

    Update main.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/ml/main.py
+++ backend/ml/main.py
@@ -1,1 +1,14 @@
+from flask import Flask, request, jsonify
+from services.recommendation import generate_recommendations
 
+app = Flask(__name__)
+
+@app.route('/recommendations', methods=['POST'])
+def recommendations():
+    data = request.json
+    user_id = data.get('user_id')
+    recommendations = generate_recommendations(user_id)
+    return jsonify(recommendations)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
