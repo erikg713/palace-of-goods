bfd35fdcd38180286b962be736a3263946f68ae1
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-29 10:42:04 -0600

    Update Onboarding.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Onboarding.js
+++ frontend/src/components/Onboarding.js
@@ -1,59 +1,20 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { completeOnboarding } from './redux/onboardingSlice';
-import { Link } from 'react-router-dom';
+import OnboardingComplete from './OnboardingComplete';
+import OnboardingForm from './OnboardingForm';
 
 const Onboarding = () => {
-  const onboardingCompleted = useSelector((state) => state.onboarding.completed);
-  const dispatch = useDispatch();
-
-  const handleComplete = () => {
-    dispatch(completeOnboarding());
-  };
+  const [showForm, setShowForm] = useState(true);
 
   return (
     <div>
-      <h1>Onboarding Page</h1>
-      {onboardingCompleted ? (
-        <p>Onboarding is already completed!</p>
+      <h1>Onboarding</h1>
+      {showForm ? (
+        <OnboardingForm toggleView={() => setShowForm(false)} />
       ) : (
-        <button onClick={handleComplete}>Complete Onboarding</button>
+        <OnboardingComplete />
       )}
-      <Link to="/">Back to Home</Link>
     </div>
   );
 };
-
-export default Onboarding;
-
-function Onboarding() {
-    const [walletAddress, setWalletAddress] = useState("");
-    const [message, setMessage] = useState("");
-
-    const handleSubmit = async () => {
-        try {
-            const response = await axios.post('http://localhost:5000/create_account', { wallet_address: walletAddress });
-            setMessage(response.data.message);
-        } catch (error) {
-            setMessage("Error: " + error.response.data.error);
-        }
-    };
-
-    return (
-        <div>
-            <h1>Onboarding</h1>
-            <input 
-                type="text"
-                value={walletAddress}
-                onChange={(e) => setWalletAddress(e.target.value)}
-                placeholder="Enter your wallet address"
-            />
-            <button onClick={handleSubmit}>Create Account</button>
-            {message && <p>{message}</p>}
-        </div>
-    );
-}
 
 export default Onboarding;
