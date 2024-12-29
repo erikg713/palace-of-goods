989213f2b3f5602ff4b2b89909782cd1ed7a0c95
Dev713 <53387865+erikg713@users.noreply.github.com>
2024-12-29 10:36:20 -0600

    Update index.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,18 @@ import { Web3Provider } from './context/Web3Context';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
 import 'react-toastify/dist/ReactToastify.css'; // Toastify CSS
 import './App.css'; // Custom styles
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import App from './App';
 
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
