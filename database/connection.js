422cb4560d8d293803a64e5d1ee5272ecf253474
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-01 03:21:11 -0600

    Create connection.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ database/connection.js
@@ -0,0 +1,17 @@
+// database/connection.js
+
+const pool = require('./config');
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    console.log('Connected to the database');
+    client.query('SELECT NOW()', (err, result) => {
+        release();
+        if (err) {
+            return console.error('Error executing query', err.stack);
+        }
+        console.log(result.rows);
+    });
+});
