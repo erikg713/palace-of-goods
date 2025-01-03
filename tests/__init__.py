bcfeff4d693287dbd408b31ceb0d74303047221d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-03 00:04:33 -0600

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,17 @@
-q
+import os
+import pytest
+from app import create_app
+from app.models import db
+
+@pytest.fixture
+def app():
+    os.environ["FLASK_ENV"] = "testing"
+    test_app = create_app()
+    with test_app.app_context():
+        db.create_all()  # Setup test database
+        yield test_app
+        db.drop_all()  # Cleanup after tests
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
