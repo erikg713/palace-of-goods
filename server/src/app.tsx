5b717ef26adb82326b8f755c3f48e806230ca449
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-24 23:48:21 -0600

    Update and rename app.ts to app.tsx

--- server/src/app.tsx
+++ server/src/app.tsx
@@ -85,3 +85,25 @@ mongoose
   });
 
 export default app;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
