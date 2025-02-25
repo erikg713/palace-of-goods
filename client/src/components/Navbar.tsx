54e34f802c1e410d6db52332d633a8050087a816
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-24 23:49:35 -0600

    Create Navbar.tsx

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Palace of Goods
+        </Typography>
+        <Button color="inherit" component={Link} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/cart">
+          Cart
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
