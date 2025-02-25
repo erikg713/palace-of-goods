4e1c2a8c70822e417b719ef35d631a4e3156c4dc
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-25 02:36:08 -0600

    Update StateManagement.tsx

--- client/src/redux/StateManagement.tsx
+++ client/src/redux/StateManagement.tsx
@@ -1,1 +1,13 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store"; // Ensure correct import of Redux store
 
+interface StateManagementProps {
+  children: React.ReactNode;
+}
+
+const StateManagement: React.FC<StateManagementProps> = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default StateManagement;
