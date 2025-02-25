b06d2bbb2e6aa8bb34632f923abd94cb87e8b407
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-25 02:54:54 -0600

    Update Index.tsx

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { UserProvider } from "./context/UserContext";
+import { PaymentProvider } from "./context/PaymentContext";
 import StateManagement from "./redux/StateManagement";
-import { AuthProvider } from "./context/AuthContext";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <StateManagement>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <UserProvider>
+        <PaymentProvider>
+          <App />
+        </PaymentProvider>
+      </UserProvider>
     </StateManagement>
   </React.StrictMode>
 );
