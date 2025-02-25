8e5a71dd4ae98a1cdaa50e35297415aa8349ee11
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-25 01:26:23 -0600

    Update store.ts

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 
+// Create Redux store
 const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== "production", // Enable DevTools in development
 });
+
+// Define RootState and AppDispatch for TypeScript
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// Custom hooks for better Redux usage in components
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
