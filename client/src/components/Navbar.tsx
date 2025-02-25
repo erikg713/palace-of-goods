d62d4a2195fda52a77f490d951596e15d24d3899
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-25 00:55:43 -0600

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -50,3 +50,36 @@ const Navbar = ({ user }) => {
 };
 
 export default Navbar;
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Avatar } from "@mui/material";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Palace of Goods
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        {user ? (
+          <>
+            <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
+            <Avatar src={`https://your-backend-url.com${user.profile_pic}`} alt="Profile" />
+            <Button color="inherit" onClick={logout}>Logout</Button>
+          </>
+        ) : (
+          <>
+            <Button color="inherit" component={Link} to="/login">Login</Button>
+            <Button color="inherit" component={Link} to="/signup">Signup</Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
