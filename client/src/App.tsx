1fe64ee759fa2924c58922462a12de29908d3be4
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-02-25 01:19:10 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import Checkout from "./pages/Checkout";
-
-const isAuthenticated = () => !!localStorage.getItem("token");
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/checkout" element={isAuthenticated() ? <Checkout /> : <Navigate to="/login" />} />
-        <Route path="/dashboard" element={isAuthenticated() ? <Dashboard /> : <Navigate to="/login" />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, AuthContext } from "./context/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
