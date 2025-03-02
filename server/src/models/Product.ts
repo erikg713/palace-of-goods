fb90148236fe7abff90eeae4258564ff1c9bae53
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-01 23:03:39 -0600

    Update Product.ts
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- server/src/models/Product.ts
+++ server/src/models/Product.ts
@@ -1,5 +1,33 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
 
+interface ProductAttributes {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  category: string;
+  sellerId: string;
+}
+
+class Product extends Model<ProductAttributes> {}
+
+Product.init(
+  {
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: false },
+    price: { type: DataTypes.FLOAT, allowNull: false },
+    image: { type: DataTypes.STRING, allowNull: false },
+    category: { type: DataTypes.STRING, allowNull: false },
+    sellerId: { type: DataTypes.UUID, allowNull: false },
+  },
+  { sequelize, modelName: "Product" }
+);
+
+export default Product;
 interface IProduct extends Document {
     name: string;
     description: string;
