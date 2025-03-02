7eba8e49ebcd6c9b5a7d62ba7d26586002494035
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-02 02:53:51 -0600

    Create service-worker.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ client/public/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "palace-of-goods-cache-v1";
+const urlsToCache = ["/", "/index.html", "/css/styles.css", "/js/main.js"];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (!cacheWhitelist.includes(cache)) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
