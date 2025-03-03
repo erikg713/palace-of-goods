544bd4425bc90f64ebd5b094384db119f20ead06
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-03 00:09:00 -0600

    Update Product.ts
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- server/src/models/Product.ts
+++ server/src/models/Product.ts
@@ -1,45 +1,17 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import { DataTypes, Model } from "sequelize";
-import sequelize from "../config/database";
 
-interface ProductAttributes {
-  id: string;
+interface IProduct extends Document {
   name: string;
   description: string;
   price: number;
-  image: string;
-  category: string;
-  sellerId: string;
-}
-
-class Product extends Model<ProductAttributes> {}
-
-Product.init(
-  {
-    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-    name: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.TEXT, allowNull: false },
-    price: { type: DataTypes.FLOAT, allowNull: false },
-    image: { type: DataTypes.STRING, allowNull: false },
-    category: { type: DataTypes.STRING, allowNull: false },
-    sellerId: { type: DataTypes.UUID, allowNull: false },
-  },
-  { sequelize, modelName: "Product" }
-);
-
-export default Product;
-interface IProduct extends Document {
-    name: string;
-    description: string;
-    price: number;
-    // ... other properties
+  // ... other properties
 }
 
 const ProductSchema: Schema = new Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    // ... other properties
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  // ... other properties
 });
 
 export default mongoose.model<IProduct>('Product', ProductSchema);
