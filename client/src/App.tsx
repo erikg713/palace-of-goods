c813d901a716e92a0e647f9b26c316a39ce15f9c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-04 05:47:30 -0600

    Update App.tsx
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,28 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/PrivateRoute";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import PrivateAdminRoute from "./components/PrivateAdminRoute";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      <Route path="/admin" element={<PrivateAdminRoute><AdminDashboard /></PrivateAdminRoute>} />
+    </Routes>
+  );
+};
+
+export default App;
 
 const App = () => {
   return (
