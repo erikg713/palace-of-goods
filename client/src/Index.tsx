ab82f5746233930c6ea3a46237f6cdc861f844c7
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-04 05:35:38 -0600

    Update Index.tsx
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -5,7 +5,22 @@ import { UserProvider } from "./context/UserContext";
 import { PaymentProvider } from "./context/PaymentContext";
 import StateManagement from "./redux/StateManagement";
 import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext"; // Provides global auth state
+import App from "./App";
+import "./styles/global.css"; // Import global styles
 
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider> {/* Wrap with Auth Context */}
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
