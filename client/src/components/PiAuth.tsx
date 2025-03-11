191d340c0fba64faa06f4dd56fb30d6a722b6bb8
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-11 00:43:37 -0500

    Update PiAuth.tsx
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- client/src/components/PiAuth.tsx
+++ client/src/components/PiAuth.tsx
@@ -53,3 +53,16 @@ const PiAuth = () => {
 };
 
 export default PiAuth;
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PiAuth: React.FC = () => {
+  return (
+    <div>
+      <p>Please log in:</p>
+      <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
+    </div>
+  );
+};
+
+export default PiAuth;
