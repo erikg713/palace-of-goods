e5b19fd64fe188c677ffc8ff0b290a7862b5cdf8
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-11 02:26:29 -0500

    Update main.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- client/public/static/js/main.js
+++ client/public/static/js/main.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Welcome to Palace of Goods!');
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to Palace of Goods!", "color: #0f62fe; font-size: 16px; font-weight: bold;");
 });
