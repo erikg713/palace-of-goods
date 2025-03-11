9cc5136b3a3a7ce89bbce58cee7c0a5d7f819053
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-03-11 03:03:47 -0500

    Create UserProfile.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,11 @@
+import userIcon from "../assets/icons/user-icon.svg";
+
+const UserProfile = () => {
+  return (
+    <button>
+      <img src={userIcon} alt="User Profile" width="24" height="24" />
+    </button>
+  );
+};
+
+export default UserProfile;
